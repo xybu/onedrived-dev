59ed55c16ad04b580eba97ca07eba99c1597b76b
Xiangyu Bu <xybu92@live.com>
2016-12-24 03:34:40 -0500

    [repo] Fixed logging issue and ignore file loading issue in OneDriveLocalRepository.
    Signed-off-by: Xiangyu Bu <xybu92@live.com>

--- onedrived/od_repo.py
+++ onedrived/od_repo.py
@@ -6,6 +6,7 @@ Core component for local file management and tracking.
 """
 
 import atexit
+import logging
 import sqlite3
 
 from . import get_resource as _get_resource
@@ -14,17 +15,18 @@ from .models.path_filter import PathFilter as _PathFilter
 
 class OneDriveLocalRepository:
 
-    def __init__(self, context, drive, drive_config):
+    def __init__(self, context, authenticator, drive, drive_config):
         """
         :param od_context.UserContext context:
+        :param od_auth.OneDriveAuthenticator authenticator:
         :param onedrivesdk.model.drive.Drive drive:
         :param models.drive_config.LocalDriveConfig drive_config:
         """
         self.context = context
+        self.authenticator = authenticator
         self.drive = drive
         self.account_id = drive_config.account_id
         self.local_root = drive_config.localroot_path
-        self.logger = context.logger
         self._init_path_filter(ignore_file=drive_config.ignorefile_path)
         self._init_item_store()
 
@@ -35,16 +37,16 @@ class OneDriveLocalRepository:
     def _init_path_filter(self, ignore_file):
         try:
             with open(ignore_file, 'r') as f:
-                rules = set(f.readlines())
+                rules = set(f.read().splitlines(keepends=False))
         except OSError as e:
-            self.logger.error('Failed to load ignore list file "%s": %s' % (ignore_file, e))
+            logging.error('Failed to load ignore list file "%s": %s', ignore_file, e)
             rules = set()
         self.path_filter = _PathFilter(rules)
 
     def _init_item_store(self):
         self._conn = sqlite3.connect(self._item_store_path, isolation_level=None, check_same_thread=False)
         self._cursor = self._conn.cursor()
-        self._cursor.execute(_get_resource('items_db.sql', pkg_name='onedrived'))
+        self._cursor.execute(_get_resource('data/items_db.sql', pkg_name='onedrived'))
         self._conn.commit()
         atexit.register(self.close)
 
