f45f83c889f2c69c838a90eb1c4fca9781c229f9
Xiangyu Bu <xybu92@live.com>
2016-12-30 00:10:30 -0500

    [models] Simpler implementation of DriveConfig.
    Signed-off-by: Xiangyu Bu <xybu92@live.com>

--- onedrived/models/__init__.py
+++ onedrived/models/__init__.py
@@ -1,3 +1,4 @@
 from . import account_profile, drive_config, path_filter, pretty_api
 
+
 __all__ = [account_profile, drive_config, path_filter, pretty_api]
--- onedrived/models/drive_config.py
+++ onedrived/models/drive_config.py
@@ -1,25 +1,8 @@
-class LocalDriveConfig:
+from collections import namedtuple
 
-    def __init__(self, drive_id, account_id, ignorefile_path, localroot_path):
-        self.data = {
-            'drive_id': drive_id,
-            'account_id': account_id,
-            'ignorefile_path': ignorefile_path,
-            'localroot_path': localroot_path
-        }
+
+class LocalDriveConfig(namedtuple('LocalDriveConfig', ('drive_id', 'account_id', 'ignorefile_path', 'localroot_path'))):
 
     @property
-    def drive_id(self):
-        return self.data['drive_id']
-
-    @property
-    def account_id(self):
-        return self.data['account_id']
-
-    @property
-    def ignorefile_path(self):
-        return self.data['ignorefile_path']
-
-    @property
-    def localroot_path(self):
-        return self.data['localroot_path']
+    def data(self):
+        return self._asdict()
