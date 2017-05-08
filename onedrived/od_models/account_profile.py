26cbbd0bcb9378663ab6a21791535323a29a7b8e
Mario Apra <mario_surfing@hotmail.com>
2017-05-08 13:47:47 -0300

    update import and change attributes order from account_profile

--- onedrived/od_models/account_profile.py
+++ onedrived/od_models/account_profile.py
@@ -1,5 +1,5 @@
 
-from .. import od_auth
+from ..od_auth import AccountTypes
 
 class OneDriveAccount:
     
@@ -11,7 +11,7 @@ class OneDriveAccount:
         return self.data['account_type']
     
     def getAccount(self):
-        if self.account_type == od_auth.AccountTypes.BUSINESS:
+        if self.account_type == AccountTypes.BUSINESS:
             return OneDriveAccountBusiness(self.data)
         else:
             return OneDriveAccountPersonal(self.data)
@@ -59,19 +59,6 @@ class OneDriveAccountBusiness:
         return self.data['id']
 
     @property
-    def account_root_folder(self):
-        return self.data['webUrl']
-
-    @property
-    def account_type(self):
-        return self.data['account_type']
-    
-    @property
-    def tenant(self):
-        site = self.account_root_folder
-        return site[8:site.find('-my.sharepoint.com/')]
-
-    @property
     def account_name(self):
         return self.data['name']
 
@@ -86,4 +73,20 @@ class OneDriveAccountBusiness:
     @property
     def account_email(self):
         return self.data['emails']
+
+    @property
+    def account_root_folder(self):
+        return self.data['webUrl']
+
+    @property
+    def account_type(self):
+        return self.data['account_type']
     
+    @property
+    def tenant(self):
+        site = self.account_root_folder
+        return site[8:site.find('-my.sharepoint.com/')]
+
+    @property
+    def endpoint(self):
+        return 'https://' + self.tenant + '-my.sharepoint.com/'
